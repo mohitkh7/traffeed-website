ec8288167396aa0630548286d47e37b0979c06b8
Mohitkh7 <mohitkh7@gmail.com>
2017-01-24 14:37:14 +0530

    Google Autcomplete

--- js/controllers/AddController.js
+++ js/controllers/AddController.js
@@ -2,7 +2,9 @@ app.controller('AddController', ['$scope', '$firebaseArray', '$location', 'FBURL
 
 	$scope.type="Traffic Jam";
 	$scope.intensity="medium";
-	
+	$scope.lat=22.7196;
+	$scope.lng=75.8577;
+
 	$scope.addNewFeed = function() {
 		var ref = new Firebase(FBURL);
 		var feeds = $firebaseArray(ref);
@@ -12,12 +14,15 @@ app.controller('AddController', ['$scope', '$firebaseArray', '$location', 'FBURL
 			location:$scope.location,
 			type:$scope.type,
 			message: $scope.message,
+			lat: $scope.lat,
+			lng: $scope.lng,
 			intensity:$scope.intensity,
-			time:$scope.time,
+			time:$scope.time
 		});
 		//$("#myModal").modal("hide");
 		$location.path('/');
 	};
+
 	$scope.draftNewFeed = function() {
 		var ref = new Firebase(FBURL3);
 		var feeds = $firebaseArray(ref);
@@ -28,10 +33,44 @@ app.controller('AddController', ['$scope', '$firebaseArray', '$location', 'FBURL
 			type:$scope.type,
 			message: $scope.message,
 			intensity:$scope.intensity,
-			time:$scope.time,
+			lat: $scope.lat,
+			lng: $scope.lng,
+			time:$scope.time
 		});
 		//$("#myModal").modal("hide");
 		$location.path('/drafts');
 	};
+
+	//Google Maps Autocomplete 
+	$scope.init=function(){
+		//Bounding in Indore
+		var southWest = new google.maps.LatLng(22.8459, 75.9504 );
+	    var northEast = new google.maps.LatLng(22.6422, 75.6460 );
+	    var indoreBounds = new google.maps.LatLngBounds( southWest, northEast );
+
+	    var options = {
+	        bounds: indoreBounds,
+	        componentRestrictions: {country: 'in'}
+	    };
+
+		var input = document.getElementById('location');
+ 		var autocomplete = new google.maps.places.Autocomplete(input, options);
+
+ 		//To be run when Changed
+ 		google.maps.event.addListener(autocomplete, 'place_changed', function () {
+     		var place = autocomplete.getPlace();
+
+			$scope.lat = place.geometry.location.lat()
+			$scope.lng = place.geometry.location.lng();
+
+			// Then do whatever you want with them
+			$scope.location=place.name;
+			input.value=place.name;
+			console.log(place.name);
+			console.log($scope.lat);
+			console.log($scope.lng);
+		});
+	}
+	$scope.init();
 
 }]);
--- js/controllers/EditController.js
+++ js/controllers/EditController.js
@@ -8,11 +8,46 @@ app.controller('EditController', ['$scope','$location', '$routeParams', '$fireba
             location:$scope.feedObj.title,
             type:$scope.feedObj.type,
             message:$scope.feedObj.message,
+            lat:$scope.feedObj.lat,
+            lng:$scope.feedObj.lng,
             intensity:$scope.feedObj.intensity,
             time:$scope.feedObj.time
         });
         $location.path('/pro');
 
     };
+
+    //Google Maps Autocomplete 
+    $scope.init=function(){
+        //Bounding in Indore
+        var southWest = new google.maps.LatLng(22.8459, 75.9504 );
+        var northEast = new google.maps.LatLng(22.6422, 75.6460 );
+        var indoreBounds = new google.maps.LatLngBounds( southWest, northEast );
+
+        var options = {
+            bounds: indoreBounds,
+            componentRestrictions: {country: 'in'}
+        };
+
+        var input = document.getElementById('location');
+        var autocomplete = new google.maps.places.Autocomplete(input, options);
+
+        //To be run when Changed
+        google.maps.event.addListener(autocomplete, 'place_changed', function () {
+            var place = autocomplete.getPlace();
+
+            $scope.feedObj.lat = place.geometry.location.lat()
+            $scope.feedObj.lng = place.geometry.location.lng();
+
+            // Then do whatever you want with them
+            $scope.feedObj.location=place.name;
+            input.value=place.name;
+            console.log(place.name);
+            console.log($scope.feedObj.lat);
+            console.log($scope.feedObj.lng);
+        });
+    }
+
+    $scope.init();
 
 }]);
